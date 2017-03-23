386540f1d40d19159cf600b270353c0bffa483a7
Rod Scott <rjsc@ceh.ac.uk>
2017-03-23 12:39:29 +0000

    Just get the installation media on the server
    master

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,55 +9,23 @@
 # === Parameters
 #
 # [*install_source*]    The location of the installation media obtainable via wget
-# [*home_directory*]    A workspace directory for storing installation media
-# [*install_directory*] The directory where fme should be installed to
-# [*user*]              who will run fme server
-# [*group*]             of the user who will run fme server
-# [*uid*]               of the user who will run fme server
-# [*gid*]               of the group who will run fme server
-# [*manage_user*]       if this module should managed the fme user
-# [*manage_group*]      if this module should managed the fme group
-# [*hostname*]          where this fme server will be run from
-# [*admin_username*]    the username used to login to fme server
-# [*admin_password*]    the password used to login to fme server
-# [*zip_version*]       the version of zip to install (false stosp install)
-# [*lsb_core_version*]  the version of lsb-core to install (false stosp install)
-# [*results_lifetime*]  the time in seconds Data Download Service files remain in DefaultResults directory
-# [*purge_interval*]    the time interval in seconds that DefaultResults directory is checked for expired content
 #
 class fmeserver (
-  $install_source    = 'http://downloads.safe.com/fme/2015/fme-server-b15515-linux-x64.run',
-  $home_directory    = '/opt/safe',
-  $install_directory = '/opt/safe/fmeserver',
-  $user              = 'fme',
-  $group             = 'fme',
-  $uid               = undef,
-  $gid               = undef,
-  $manage_user       = true,
-  $manage_group      = true,
-  $hostname          = $::fqdn,
-  $admin_username    = 'admin',
-  $admin_password    = 'password',
-  $zip_version       = installed,
-  $lsb_core_version  = installed,
-  $results_lifetime  = 2592000,
-  $purge_interval    = 43200,
+  $install_source = undef,
 ) {
 
-  if $manage_user {
-    # Ensure the fme user is present
-    user { $user :
-      ensure => present,
-      uid    => $uid,
-      gid    => $group,
+  include wget
+  if $install_source {
+    wget::fetch { $install_source :
+      destination => '/tmp/fme-server.run',
     }
   }
 
-  if $manage_group {
-    # Ensure the fme group is present
-    group { $group :
-      ensure => present,
-      gid    => $gid,
-    }
+  package { 'lsb-core' :
+    ensure => present,
+  }
+
+  package { 'zip' :
+    ensure => present,
   }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,12 +2,4 @@ require 'spec_helper'
 
 describe 'fmeserver' do
   it { is_expected.to compile }
-  it { is_expected.to contain_user('fme') }
-  it { is_expected.to contain_group('fme') }
-  it { is_expected.to contain_service('FMEServerAppServer') }
-  it { is_expected.to contain_service('FMEServerCleanup') }
-  it { is_expected.to contain_service('FMEServerCore') }
-  it { is_expected.to contain_service('FMEServerDatabase') }
-  it { is_expected.to contain_service('FMEServerEngines') }
-  it { is_expected.to contain_service('FMEServerWebSocket') }
 end
